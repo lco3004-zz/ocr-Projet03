39da521e435c596bd2c976d522f00448fcea9395
laurent cordier <laurent.cordier3004@gmail.com>
2019-03-01 00:30:40 +0100

    simplification gestionparametre ok fonctionne

--- src/main/java/ocr_projet03/paramsOcr_Projet03/paramsMM/GroupParamsMM.java
+++ src/main/java/ocr_projet03/paramsOcr_Projet03/paramsMM/GroupParamsMM.java
@@ -3,22 +3,22 @@ package ocr_projet03.paramsOcr_Projet03.paramsMM;
 import ocr_projet03.paramsOcr_Projet03.UnParam;
 
 public enum GroupParamsMM {
-        NbDePositions( (Integer)4,(Integer)3,(Integer)6),
-        NbCouleurs((Integer)CouleursMastermind.values().length,(Integer)6,(Integer)CouleursMastermind.values().length),
-        NbreMaxDeBoucleChercheCodeSecret((Integer)100,(Integer)10,(Integer)500),
-        NbreEssais((Integer)12,(Integer)8,(Integer)12),
-        DoublonAutorise((Boolean)false),
-        CaseVideAUtorise ((Boolean)false);
+        NbDePositions( 4,3,6),
+        NbCouleurs(CouleursMastermind.values().length,6,CouleursMastermind.values().length),
+        NbreMaxDeBoucleChercheCodeSecret(100,10,500),
+        NbreEssais(12,8,12),
+        DoublonAutorise(false),
+        CaseVideAUtorise (false);
 
 
         private UnParam unParam;
 
         GroupParamsMM(Integer valdef, Integer valmin,Integer valmax) {
-            unParam = new UnParam<Integer>(valdef, Integer.class.getSimpleName(),0,7);
+            unParam = new UnParam<>(valdef, Integer.class.getSimpleName(),valmin,valmax);
         }
 
         GroupParamsMM(Boolean valdef) {
-            unParam = new UnParam<Boolean>(valdef,Boolean.class.getSimpleName());
+            unParam = new UnParam<>(valdef,Boolean.class.getSimpleName());
         }
 
      public UnParam getUnParam() {
