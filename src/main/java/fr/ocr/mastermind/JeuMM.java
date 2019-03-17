008d63c30bf3dbf7dfdd8d8f0b043e54ad7f56c7
laurent cordier <laurent.cordier3004@gmail.com>
2019-03-17 18:16:47 +0100

    ajout notes.txt pour rélisation mode defenseur

--- src/main/java/fr/ocr/mastermind/JeuMM.java
+++ src/main/java/fr/ocr/mastermind/JeuMM.java
@@ -6,6 +6,7 @@ import fr.ocr.utiles.AppExceptions;
 import fr.ocr.utiles.Constantes;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import static fr.ocr.utiles.Constantes.ConstEvalPropale.BLANC_MALPLACE;
@@ -101,6 +102,47 @@ public class JeuMM implements ValidationPropale {
         return zoneEvaluation[NOIR_BIENPLACE] == nombreDePositions;
     }
 
+    /*
+
+     */
+
+    /**
+     *
+     * @param number nombre a convertir (sous forme de chaine)
+     * @param sBase  base du nombre à convertir
+     * @param dBase  base de destination
+     * @return
+     */
+    public  String baseConversion(String number,
+                                  int sBase, int dBase)
+    {
+        // Parse the number with source radix
+        // and return in specified radix(base)
+        return Integer.toString(Integer.parseInt(number, sBase), dBase);
+    }
+    /**
+     *       Sc(i,j)
+     *       I   J
+     *       0 (0..nbPos)
+     *       1 (0..nbPos-1)
+     *       2 (0..nbPos-2)
+     *       3 (0)
+     *       4 (0..nbPos-4) 0..0
+     * @param nbPos
+     * @return List< Integer [] >
+     */
+    public static List< Integer [] > LesScoresPossibles (int nbPos) {
+        List<Integer [] > scPossible = new ArrayList<Integer [] >(256);
+        for (int noirs =0; noirs < nbPos -1 ;noirs++) {
+            for (int blancs = 0; blancs <= nbPos - noirs; blancs++) {
+
+                scPossible.add(new Integer[]{noirs, blancs});
+            }
+        }
+        scPossible.add(new Integer[] {nbPos-1, 0});
+        scPossible.add(new Integer[] {nbPos, 0});
+        return scPossible;
+    }
     /**
      * @return le code secret (String)
      */
