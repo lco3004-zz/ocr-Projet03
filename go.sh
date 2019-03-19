7b94742f21872d713475b37c4f8a3a207e997697
Laurent Cordier <laurent.cordier3004@gmail.com>
2019-03-19 19:00:58 +0100

    preparation "Ordinateur Decode" Mode defenseur

--- go.sh
+++
@@ -1,6 +0,0 @@
-
-#! /bin/sh
-monchemin=`pwd`/target/
-echo $monchemin
-java.exe -Dfile.encoding=windows-1252 -jar $monchemin/projet03-1.0.jar
-
--- src/main/java/fr/ocr/mastermind/JeuMMDefenseur.java
+++ src/main/java/fr/ocr/mastermind/JeuMMDefenseur.java
@@ -3,7 +3,6 @@ package fr.ocr.mastermind;
 import fr.ocr.modeconsole.MenuSaisieSecret;
 import fr.ocr.utiles.Constantes.Libelles.LibellesMenuSecondaire;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,32 +15,6 @@ public class JeuMMDefenseur extends JeuMM {
         super(modeJeu, sc);
 
     }
-
-    /**
-     * Sc(i,j)
-     * I   J
-     * 0 (0..nbPos)
-     * 1 (0..nbPos-1)
-     * 2 (0..nbPos-2)
-     * 3 (0)
-     * 4 (0..nbPos-4) 0..0
-     *
-     * @param nbPos Integer , le nombre de positions dans une ligne du jeu MM
-     * @return List<Integer [ ]> les scores possibles qui peuvent être obtenus par une proposition
-     */
-    public static List<Integer[]> CalculScoresPossibles(int nbPos) {
-        List<Integer[]> scPossible = new ArrayList<>(256);
-        for (int noirs = 0; noirs < nbPos - 1; noirs++) {
-            for (int blancs = 0; blancs <= nbPos - noirs; blancs++) {
-
-                scPossible.add(new Integer[]{noirs, blancs});
-            }
-        }
-        scPossible.add(new Integer[]{nbPos - 1, 0});
-        scPossible.add(new Integer[]{nbPos, 0});
-        return scPossible;
-    }
-
     public void runJeuMM() {
         MenuSaisieSecret menuSaisieSecret = new MenuSaisieSecret();
         FabricationSecretMM fabricationSecretMM = new FabricationSecretMM(menuSaisieSecret.saisirCombinaisonSecrete());
@@ -49,19 +22,6 @@ public class JeuMMDefenseur extends JeuMM {
         this.validerProposition = new EvalPropaleDefenseur();
 
         super.RunJeuMMDefenseur(fabricationSecretMM);
-    }
-
-    /**
-     * @param number nombre a convertir (sous forme de chaine)
-     * @param sBase  base du nombre à convertir
-     * @param dBase  base de destination
-     * @return chaine nombre en base dBase
-     */
-    public String baseConversion(String number,
-                                 int sBase, int dBase) {
-        // Parse the number with source radix
-        // and return in specified radix(base)
-        return Integer.toString(Integer.parseInt(number, sBase), dBase);
     }
 
 }
